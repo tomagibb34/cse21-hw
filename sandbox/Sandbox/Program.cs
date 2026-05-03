798db7a3766a1ad7ffbb8f1dc2d254f623a84419
tomagibb34 <tomagibb@gmail.com>
2026-05-02 18:23:10 -0700

    Updates to satisfy the Exercise 1 programming requirement.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Feel free to experiment with your code here.");
     }
 }
