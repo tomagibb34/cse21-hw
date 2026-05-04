f462f5b1a0bccb2d5dc162fca53de9f3e922899f
tomagibb34 <tomagibb@gmail.com>
2026-05-03 22:54:38 -0700

    Completed Exercise 3 and uploading code.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
+// CSE 210 - Introduction to Programming
+// Exercise 3 - Magic Number guessing game
+// Author: Tom Gibb
+// Date: 03-05-2026
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("This is the Magic Number Guessing Game exercise with a number range of 1 to 100.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        int guessInteger = -1;
+
+        while (guessInteger != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessInteger = int.Parse(Console.ReadLine());
+            if (guessInteger < magicNumber)
+            {
+                Console.WriteLine("Your guess is too low. Try again!");
+            }
+            else if (guessInteger > magicNumber)
+            {
+                Console.WriteLine("Your guess is too high. Try again!");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! The magic number was {magicNumber}");
+            }
+        }
     }
 }
